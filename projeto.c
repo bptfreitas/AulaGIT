7061d30748b5f06365db52634f950a99c278d464
bptfreitas <brunopolicarpo@hotmail.com>
2019-11-07 21:59:30 -0300

    Update projeto.c

--- projeto.c
+++ projeto.c
@@ -2,12 +2,203 @@
 
 using namespace std;
 
-void funcao_Professor(){
-	cout << "Meu nome eh Bruno";
+void funcaoANNA_CHAZAN()
+{
+}
+
+void funcaoARTUR_OLIVEIRA()
+{
+}
+
+void funcaoCAIO_FARIAS()
+{
+}
+
+void funcaoCAIO_SIQUEIRA()
+{
+}
+
+void funcaoEDUARDO_SILVA()
+{
+}
+
+void funcaoGABRIEL_SANTOS()	
+{
+}
+
+void funcaoGUILHERME_ROSA()	
+{
+}
+
+void funcaoJOÃO_CONCEIÇÃO()
+{
+}
+
+void funcaoLUCAS_LIMA()
+{
+}
+
+void funcaoLUIZA_ELLER()	
+{
+}
+
+void funcaoLUIZA_FRIAS()	
+{
+}
+
+void funcaoMARCO_NETTO()	
+{
+}
+
+void funcaoMARIA_PACHECO()	
+{
+}
+
+void funcaoMARIA_COIMBRA()	
+{
+}
+
+void funcaoREBECA_PEREIRA()	
+{
+}
+
+void funcaoREBECA_FERNANDES()
+{
+}
+
+void funcaoSEBASTIÃO_NETO()
+{
+}
+
+void funcaoTHAIS_WERMELINGER()
+{
+}
+
+void funcaoVICTOR_LOMBA()
+{
+}
+
+void funcaoWELLINGTON_PEDRO()	
+{
+}
+
+void funcaoYURI_SILVA()	
+{
+}
+
+void funcaoANA_PORTO()
+{
+}
+
+void funcaoCALEBE_LUZ()	
+{
+}
+
+void funcaoCARLOS_BANJAR()
+{
+}
+
+void funcaoCAUA_JUNGER()	
+{
+}
+
+void funcaoDOUGLAS_RIBEIRO()
+{
+}
+
+void funcaoEVELLYN_ORTEGA()
+{
+}
+
+void funcaoFILIPE_EVANGELISTA()
+{
+}
+
+void funcaoGABRIEL_FERNANDES()
+{
+}
+
+void funcaoIAN_BIANQUINI()
+{
+}
+
+void funcaoISABELLA_AZEVEDO()
+{
+}
+
+void funcaoJOANA_SANTOS()]
+{
+}
+
+void funcaoKAYLANNE_VIEIRA()
+{
+}
+
+void funcaoLARISSA_SCHUMACKER()
+{
+}
+
+void funcaoLUCAS_TULER()
+{
+}
+
+void funcaoLUCAS_BADINI()
+{
+}
+
+void funcaoLUCIUS_SILVA()
+{
+}
+
+void funcaoMARIA_SANGLARD()
+{
+}
+
+void funcaoMARIA_MARINHO()
+{
+}
+
+void funcaoMARIA_JIQUIRIÇÁ()
+{
+}
+
+void funcaoMARIANA_FARIA()
+{
+}
+
+void funcaoMAYSA_AQUINO()
+{
+}
+
+void funcaoMYLENA_PIMENTEL()
+{
+}
+
+void funcaoPEDRO_CARDINOT()
+{
+}
+
+void funcaoRAFAEL_BADINI()
+{
+}
+
+void funcaoRHAYAN_FREITAS()
+{
+}
+
+void funcaoSAULO_NERY()
+{
+}
+
+void funcaoTHIAGO_OLIVEIRA()
+{
+}
+
+void funcaoVICTOR_STUTZ()
+{
 }
 
 int main(){
-	
 	
   cout << "Olá mundo!";
   
