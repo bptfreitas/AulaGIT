4a5d0bc1a556f3119536becd8e294cf0a0ba4f40
BPTF <brunopolicarpo@hotmail.com>
2021-04-05 01:26:17 -0300

    Antes do conflito

--- projeto.c
+++ projeto.c
@@ -3,16 +3,9 @@
 using namespace std;
 
 int funcao(){
-
 	printf("Outra frase");
-
 	return 0;
 }
-
-int Funcao3(){
-	printf("Funcao2");
-	
-}	
 
 
 int main(){
@@ -22,3 +15,4 @@ int main(){
 
 	return  0;
 }
+
